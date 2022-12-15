462b129d1bed0136ff81b34304f3f8c0d41a9ddf
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2022-12-15 16:02:31 -0300

    Segundo test, modificaciones

--- src/components/404/Error404.js
+++ src/components/404/Error404.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../Layout/Layout.css'
+
+
 
 const Error404 = () => {
     return (
-        <h2>Error 404 not found</h2>
+        <div className='content flex flex-col items-center'>
+            <img src='../../error404.svg' alt='Imagen error 404' className='w-64 h-64 m-0'></img>
+            <h2 className='text-slate-300 text-6xl'>Error 404 not found</h2>
+            <p className='text-slate-300 text-2xl m-6'>The resource requested could not be found on this server!</p>
+        </div>
     )
 }
 
--- src/components/ItemDetailsContainer/ItemDetailsContainer.js
+++ src/components/ItemDetailsContainer/ItemDetailsContainer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ItemDetailsContainer = () => {
+
+    const {id} = useParams()
+
     return (
-        <div>ItemDetailsContainer</div>
+        <h1>
+            Se cargo el componente con el id {id}
+        </h1>
     )
 }
 
