62dafa619843f95ca633ee477a3ae128281b97fb
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2022-12-20 10:47:56 -0300

    Entregable n2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import CartWidget from './components/CartWidget/CartWidget';
-import Checkout from './components/Checkout/Checkout';
-import Error404 from './components/404/Error404';
-import ItemDetailsContainer from './components/ItemDetailsContainer/ItemDetailsContainer'
-import Layout from './components/Layout/Layout';
-=======
-import CartWidget from './components/Global/NavBar/CartWidget/CartWidget';
 import Error404 from './components/Global/PageNotFound/Error404';
 import Layout from './components/Global/Layout/Layout';
 import ItemListContainer from './components/Products/ItemListContainer';
 import ItemDetailContainer from './components/Products/ItemDetailContainer';
->>>>>>> itemlist
+import CartWidget from './components/Global/NavBar/CartWidget/CartWidget';
 
 
 
@@ -26,16 +17,9 @@ function App() {
 			<Layout>
 				<Routes>
 					<Route path='/' element={<ItemListContainer/>}/>
-<<<<<<< HEAD
-					<Route path='/category/:id' element={<ItemListContainer/>} />
-					<Route path='/item/detail/:id' element={<ItemDetailsContainer/>} />
-					<Route path='/cart' element={<CartWidget/>} />
-					<Route path='/checkout' element={<Checkout/>} />
-=======
 					<Route path='/category/:categoria' element={<ItemListContainer/>} />
 					<Route path='/item/detail/:id' element={<ItemDetailContainer/>} />
 					<Route path='/cart' element={<CartWidget/>} />
->>>>>>> itemlist
 					<Route path='*' element={<Error404/>} />
 				</Routes>
 			</Layout>
--- src/components/Products/ItemListContainer.jsx
+++ src/components/Products/ItemListContainer.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import config from '../../config.json';
-<<<<<<< HEAD:src/components/ItemListContainer/ItemListContainer.js
-import Spinner from '../UI/Spinner/Spinner.js';
-import Card from '../Card/Card.js';
-import './ItemListContainer.css'
-import '../Layout/Layout.css'
-=======
 import Item from './Item';
 import Spinner from '../UI/Spinner/Spinner'
 import './styles/ItemListContainer.css'
->>>>>>> itemlist:src/components/Products/ItemListContainer.jsx
 
 
 const ItemListContainer = () => {
@@ -52,19 +45,10 @@ const ItemListContainer = () => {
 		}
 	}, [])
 
-<<<<<<< HEAD:src/components/ItemListContainer/ItemListContainer.js
-    return (
-    <div className="content__products"> 
-        {cards.length < 1}
-        <section className="flex flex-row flex-wrap justify-center mx-64 my-16 gap-6">
-=======
-
-
     return ( 
 	<section className='products'>
         {cards.length < 1}
         <div className="products__container flex flex-row flex-wrap justify-center mx-96 my-16 h-full">
->>>>>>> itemlist:src/components/Products/ItemListContainer.jsx
             {loading && <Spinner/>}
             {!loading && cards.length > 0 ? filter.map(({id, title, description, price, offer, img, alt, stock},index) => (
                 <Item
@@ -80,16 +64,9 @@ const ItemListContainer = () => {
                 />) 
                 ) : !loading && cards.length < 1 && (<h1 className= "text-center text-red-800 text-xl">Ups!, fallo la carga de productos</h1>)
             }
-<<<<<<< HEAD:src/components/ItemListContainer/ItemListContainer.js
-        </section>
-        <Title greeting= 'Listado de productos'/>
-    </div>
-
-=======
         </div>
         {/* <Title greeting= 'Listado de productos'/> */}
 	</section>
->>>>>>> itemlist:src/components/Products/ItemListContainer.jsx
     )
 }
 
