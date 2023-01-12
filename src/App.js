cfc5369299d3e448e3922cc76fc91d722640d465
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-01-12 01:54:07 -0300

    Reestructurando arquitectura

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Error404 from './components/Global/PageNotFound/Error404';
-import Layout from './components/Global/Layout/Layout';
-import ItemListContainer from './components/Products/ItemListContainer';
-import ItemDetailContainer from './components/Products/ItemDetailContainer';
+import Layout from './layout/Layout'
 import CarritoContextProvider from './context/CarritoContext';
-import Cart from './components/Cart/Cart';
+import Rutas from './rutas/Rutas';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 
 function App() {
 	return (
-
-		<Router>
-			<CarritoContextProvider>
-				<Layout>
-					<Routes>
-						<Route path='/' element={<ItemListContainer/>}/>
-						<Route path='/category/:categoria' element={<ItemListContainer/>} />
-						<Route path='/item/detail/:id' element={<ItemDetailContainer/>} />
-						<Route path='/cart' element={<Cart/>} />
-						<Route path='*' element={<Error404/>} />
-					</Routes>
-				</Layout>
-			</CarritoContextProvider>
-		</Router>
+		<>
+			<Router>
+				<CarritoContextProvider>
+					<Layout>
+						<Rutas/>
+					</Layout>
+				</CarritoContextProvider>
+			</Router>
+		</>
 	);
 }
 
--- src/exports/banners.js
+++
@@ -1,4 +0,0 @@
-import image1 from '../assets/ImagenesBanners/Apple_MacBookPro_14.jpg'
-import image2 from '../assets/ImagenesBanners/Asus_notebookgamer.jpg'
-
-export default [image1, image2]
